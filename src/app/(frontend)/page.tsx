8bfe349ddae970b6ed8ef3856247af635046fec0
Adrian Maj <adrianmaj1122@gmail.com>
2026-09-06 16:01:40 +0200

    feat: finished setup

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
   return (
-    <section className="bg-primary">
-      <h1 className="text-secondary">Welcome to PayloadCMS Boilerplate!</h1>
+    <section className="flex min-h-screen flex-col items-center justify-center gap-4 bg-primary">
+      <h1 className="mx-auto text-secondary">Welcome to PayloadCMS Boilerplate!</h1>
+      <Button asChild variant="secondary">
+        <Link href="/admin">Go to Admin</Link>
+      </Button>
     </section>
   );
 };
